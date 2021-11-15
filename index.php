e6fc672b68457ca6c1b3841cff129dba1d3e69bd
Guillaume Baur <guilaume.baur@gmail.com>
2021-11-15 12:12:28 +0100

    ajout_aside

--- index.php
+++ index.php
@@ -32,11 +32,33 @@
 	</header>
 	<div class="slogan">bienvenu chez vous !</div>
 
+	<aside>
+		<ul>
+			<li>2 voyageurs</li>
+			<li>1 chambre</li>
+			<li>1 lit</li>
+			<li>1 salle de bain privée</li>
+		</ul>
+	</aside>
+
 	<main>
 			<img src="PHOTOS/CHAMBRE 1/ACCUEIL.jpg"/>
 	</main>
 
 	<footer>
+		<span class="footerBlock">
+			<div>
+				<i class="material-icons">home  </i>
+				<a href="https://www.airbnb.fr/rooms/50823599/location?source_impression_id=p3_1636968589_EEJpurRhqFc31Nm8&guests=1&adults=1">Gigean, Occitanie, France</a>
+			<div>
+				<i class="material-icons">settings_phone</i>
+				<a href="tel:0678594959">Tél: </a>
+			</div>
+			<div>
+				<i class="material-icons">email</i>
+				<a href="mailto:adresse@gmail.com">adresse@gmail.com</a>
+			</div>
+		</span>
 	</footer>
 
 	</body>
