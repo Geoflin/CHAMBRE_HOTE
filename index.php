a7ce8e8fc0191fc4979cffbb0b37ba9015828496
Guillaume Baur <guilaume.baur@gmail.com>
2021-11-27 09:14:03 +0100

    php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 		<link href="CSS/diaporama.css" rel="stylesheet" type="text/css" />
 		<link href="CSS/class.css" rel="stylesheet" type="text/css" />
 		<link href="CSS/img.css" rel="stylesheet" type="text/css" />
+		<link href="CSS/traduction.css" rel="stylesheet" type="text/css" />
 
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	</head>
@@ -25,47 +26,51 @@
 	<header>
 			<div class="mainTitle">
 			<img src="PHOTOS/GRAPPE.png" height="90px"/>
-			<h1>Maison vigneronne au cœur du village</h1>
-			<div id="translator_6086"><noscript>Javascript is disable - <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/6086-3-ynnynnnnn.js"></script>
+			<h1>Maison vigneronne au cœur du village<en>Winegrower's house in the heart of the village</en></h1>
+
+			<div class="element selected">
+                <div id="translator_3"><a style="cursor:pointer;margin:1px;display:inline-block;" onclick="translate_page('en');"><div style="width: 50px; height: 50px; background: url(&quot;http://services.supportduweb.com/translator/styles/images/s3.png&quot;) 0px 0px; display: inline-block; padding: 0px; margin: 0px;" onmouseover="this.style.backgroundPosition='-0px -50px';" onmouseout="this.style.backgroundPosition='-0px 0px';" alt="English">
+				</div><a style="cursor:pointer;margin:1px;display:inline-block;" onclick="translate_page('fr');"><div style="width: 50px; height: 50px; background: url(&quot;http://services.supportduweb.com/translator/styles/images/s3.png&quot;) -150px 0px; display: inline-block; padding: 0px; margin: 0px;" onmouseover="this.style.backgroundPosition='-150px -50px';" onmouseout="this.style.backgroundPosition='-150px 0px';" alt="Français">
 			</div>
+
 	</header>
 
 	<nav>
-		<div><a href="index.php">ACCUEIL</a></div>
+		<div><a href="index.php"><en>HOME</en>ACCUEIL</a></div>
 		<div>
 			<ul id="menu-deroulant">
-				<li><a href="#">CHAMBRE</a>
+				<li><a href="#"><en>BEDROOM</en>CHAMBRE</a>
 					<ul>
-						<li><a href="chambre_azur.php" id="azur">Chambre Azur</a></li>
-						<li><a href="chambre_or.php" id="or">Chambre Or</a></li>
+						<li><a href="chambre_azur.php" id="azur"><en>Azure Room</en>Chambre Azur</a></li>
+						<li><a href="chambre_or.php" id="or"><en>Gold Room</en>Chambre Or</a></li>
 					</ul>
 			</ul>
 		</div>
-		<div><a href="jardin_piscine.php">JARDIN/PISCINE</a></div>
-		<div><a>REGION</a></div>
+		<div><a href="jardin_piscine.php"><en>GARDEN/POOL</en>JARDIN/PISCINE</a></div>
+		<div><a><en>REGION</en>REGION</a></div>
 </nav>
 
 	<main>
 			<img src="PHOTOS/COMMUN/JARDIN.jpg" class="img"/>
-			<div class="style">Ouvert toute l'année</div> 
-			<p>Bruno et David vous accueillent dans leur charmante vigneronne datant de 1860, entièrement restaurée. Elle est idéalement située entre Montpellier et Sète, proche de l'étang de Thau et des plages. Nous mettons à votre disposition une suite de 33m², chambre avec lit de 180 cm et salle de bain privative avec WC. L' accès se fait par un petit escalier en colimaçon. Petit déjeuner inclus. Parking public gratuit.</p>
+			<div class="style"><en>Open all year</en>Ouvert toute l'année</div> 
+			<p><en>Bruno and David welcome you to their charming winegrower dating from 1860, completely restored. It is ideally located between Montpellier and Sète, close to the Thau lagoon and the beaches. We provide you with a 33m² suite, bedroom with 180cm bed and private bathroom with WC. Access is via a small spiral staircase. Breakfast included. Free public parking.</en>Bruno et David vous accueillent dans leur charmante vigneronne datant de 1860, entièrement restaurée. Elle est idéalement située entre Montpellier et Sète, proche de l'étang de Thau et des plages. Nous mettons à votre disposition une suite de 33m², chambre avec lit de 180 cm et salle de bain privative avec WC. L' accès se fait par un petit escalier en colimaçon. Petit déjeuner inclus. Parking public gratuit.</p>
 
-			<h2>Nos points forts</h2>
+			<h2><en>Our strong points</en>Nos points forts</h2>
 
 
-			<h3>Ouvert toute l'année !</h3>
+			<h3><en>Open all year !</en>Ouvert toute l'année !</h3>
 
 			<div class="points_fort">
-				<div class="Ambiance" id="Ambiance">Ambiance</div>
-				<div>Confiance</div>
-				<div>Situation</div>
+				<div class="Ambiance" id="Ambiance"><en>Atmosphere</en>Ambiance</div>
+				<div><en>Confidence</en>Confiance</div>
+				<div><en>position</en>Situation</div>
 			</div>
 
 			<section class="Ambiance">
-				<p class="Ambiance">Calme en coeur de village</p>
+				<p class="Ambiance"><en>Quiet in the heart of the village</en>Calme en coeur de village</p>
 			</section>
 
-			<h2>Ils parlent de nous !</h2>
+			<h2><en>REGION</en>Ils parlent de nous !</h2>
 			<h1><div class="airbnb-embed-frame" data-id="50823599" data-view="home" style="width:450px;height:300px;margin:auto"><a href="https://www.airbnb.fr/rooms/50823599?guests=1&amp;amp;adults=1&amp;amp;s=66&amp;amp;source=embed_widget">Voir sur Airbn</a><a href="https://www.airbnb.fr/rooms/50823599?guests=1&amp;amp;adults=1&amp;amp;s=66&amp;amp;source=embed_widget" rel="nofollow">Maison vigneronne au cœur du village 1</a><script async="" src="https://www.airbnb.fr/embeddable/airbnb_jssdk"></script></div></h1>
 
 	</main>
