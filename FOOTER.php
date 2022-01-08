efcd713eada5d8ecf09b09012e170fa02723efc3
Guillaume Baur <guilaume.baur@gmail.com>
2022-01-08 15:23:53 +0100

    numero_telephone_bruno

--- FOOTER.php
+++ FOOTER.php
@@ -1,6 +1,6 @@
 <i class="material-icons">home  </i>
 				<a href="https://goo.gl/maps/22sXuBSoqTjjwuyGA">Gigean, Occitanie, France</a>
 				<i class="material-icons">settings_phone</i>
-				<a href="tel:0678594959">Tél: 06.84.66.25.22</a>
+				<a href="tel:0684662522">Tél: 06.84.66.25.22</a>
 				<i class="material-icons">email</i>
 				<a href="mailto:lavigneronnedegigean@gmail.com">lavigneronnedegigean@gmail.com</a>
